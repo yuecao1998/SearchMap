5f4d4016e7f8afd65b0c5209efe48af947186c24
Lily Cao <cao517@usc.edu>
2018-09-16 03:30:36 -0700

    add more test cases and .gitignore

--- src/TestFlightMap.java
+++ src/TestFlightMap.java
@@ -22,7 +22,34 @@ public class TestFlightMap {
 	}
 	
 	
-
+	@org.junit.Test
+	public void testGetRoute() {
+		FlightMap map = new FlightMap();
+		List<String> content = new ArrayList<>();
+		content.add("P W 200");
+		content.add("P R 300");
+		content.add("W S 200");
+		map.getMap(content);
+		Set<Character> arrived = new HashSet<>();
+		List<String> route = new ArrayList<>();
+		map.getRoute('P', "P", arrived, route);
+		assertEquals(route.get(0), "PR");
+		assertEquals(route.get(1), "PW");
+		assertEquals(route.get(2), "PWS");	
+	}
+	
+	@org.junit.Test
+	public void testGetCost() {
+		FlightMap map = new FlightMap();
+		List<String> content = new ArrayList<>();
+		content.add("P W 200");
+		content.add("P R 300");
+		content.add("W S 200");
+		content.add("S T 100");
+		map.getMap(content);
+		assertEquals(map.getCost("PW"), 200);
+		assertEquals(map.getCost("PWS"), 400);
+	}
 
 
 
